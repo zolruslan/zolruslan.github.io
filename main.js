33d993b08cd33d438745f0c8e7b441d225a25d39
zolruslan <zolruslan@ukr.net>
2017-10-25 21:13:56 +0300

    site

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	$('#button1').click(function (){
+		var File = "1.txt";
+		request(File);
+	})
+	$('#button2').click(function (){
+		var File = "2.txt";
+		request(File);
+	})
+	function request (data){
+		$.ajax({
+			url:data,
+			sucsess:function(data){
+				$('#text').html(data);
+			}
+		})
+	}
+})
